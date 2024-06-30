453f11a8ca5fdb1b15af458ee0a85260fb3b4b53
kasevvlad <vladkasev13@gmail.com>
2024-06-30 12:47:10 +0500

    fix some code

--- app/SpeedTest.php
+++ app/SpeedTest.php
@@ -22,20 +22,17 @@ class SpeedTest
 
     public function run()
     {
-        while (true) {
-            $downloadSpeed = $this->check_speed_download();
-            $uploadSpeed = $this->check_speed_upload();
-            $pingSpeed = $this->check_speed_ping();
-            if ($downloadSpeed !== null && $downloadSpeed < 300) {
-                $this->sendDiscordNotification("Attention! download below 300 Mbps. Current download " . $downloadSpeed . " Mbit/s");
-            }
-            if ($uploadSpeed !== null && $uploadSpeed < 300) {
-                $this->sendDiscordNotification("Attention! upload below 300 Mbps. Current upload " . $uploadSpeed . " Mbit/s");
-            }
-            if ($pingSpeed !== null && $pingSpeed < 20) {
-                $this->sendDiscordNotification("Attention! ping below 20. Current ping " . $pingSpeed);
-            }
-            sleep(3600);
+        $downloadSpeed = $this->check_speed_download();
+        $uploadSpeed = $this->check_speed_upload();
+        $pingSpeed = $this->check_speed_ping();
+        if ($downloadSpeed !== null && $downloadSpeed < 300) {
+            $this->sendDiscordNotification("Attention! download below 300 Mbps. Current download " . $downloadSpeed . " Mbit/s");
+        }
+        if ($uploadSpeed !== null && $uploadSpeed < 300) {
+            $this->sendDiscordNotification("Attention! upload below 300 Mbps. Current upload " . $uploadSpeed . " Mbit/s");
+        }
+        if ($pingSpeed !== null && $pingSpeed < 20) {
+            $this->sendDiscordNotification("Attention! ping below 20. Current ping " . $pingSpeed);
         }
     }
 
